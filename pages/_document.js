b87915373b3f88b6dc62db90e7a4ee09b3d87813
FoysalAhammed <hanif.mia2233@gmail.com>
2024-12-21 19:22:46 +0600

    lenis updated

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script src="https://unpkg.com/lenis@1.1.14/dist/lenis.min.js" />
       </Head>
       <body>
         <Main />
